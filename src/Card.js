574e8efe9d99be5c8fae1f3abf6bd1151deb9624
Felicia Udosen <udosenfelicia@gmail.com>
2022-08-05 09:46:00 +0100

    initail commit

---
+++ src/Card.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='card'>
+      <p>LAST 7 DAYS</p>
+      <p><span>2</span>ib</p>
+    </div>
+  )
+}
+
+export default Card
